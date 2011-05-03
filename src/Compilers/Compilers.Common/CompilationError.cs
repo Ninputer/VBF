04d378f48522ae9c77fd6b1e0952f25cdf1fe73b
Fan Shi <ninputer@hotmail.com>
2011-05-03 19:23:12 +0800

    Adds CompilationError.cs

---
+++ src/Compilers/Compilers.Common/CompilationError.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VBF.Compilers
+{
+    public class CompilationError
+    {
+        public int Id { get; private set; }
+        public int Level { get; private set; }
+
+    }
+}
