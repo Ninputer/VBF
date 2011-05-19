ba106eac22c12e939dd69c186dd4fb659d513649
Fan Shi <ninputer@hotmail.com>
2011-05-20 01:43:34 +0800

    Adds mini sharp keywords.

--- src/Samples/MiniSharp/MiniSharpParser.cs
+++ src/Samples/MiniSharp/MiniSharpParser.cs
@@ -56,6 +56,7 @@ namespace VBF.MiniSharp
         private Token COMMA; // ,
         private Token COLON; // :
         private Token SEMICOLON; // ;
+        private Token DOT; // .
 
         private Token WHITESPACE;
         private Token COMMENT;
@@ -63,21 +64,44 @@ namespace VBF.MiniSharp
 
         protected override void OnDefineLexer(Compilers.Scanners.Lexicon lexicon, ICollection<int> skippedTokens)
         {
+            var lex = lexicon.DefaultLexer;
 
-            var lettersCategories = new[] { UnicodeCategory.LetterNumber,
-                                            UnicodeCategory.LowercaseLetter,
-                                            UnicodeCategory.ModifierLetter,
-                                            UnicodeCategory.OtherLetter,
-                                            UnicodeCategory.TitlecaseLetter,
-                                            UnicodeCategory.UppercaseLetter};
+            var lettersCategories = new[] 
+            { 
+                UnicodeCategory.LetterNumber,
+                UnicodeCategory.LowercaseLetter,
+                UnicodeCategory.ModifierLetter,
+                UnicodeCategory.OtherLetter,
+                UnicodeCategory.TitlecaseLetter,
+                UnicodeCategory.UppercaseLetter
+            };
 
-            var RE_IDCHAR = RE.CharsOf(c => lettersCategories.Contains(Char.GetUnicodeCategory(c)));
+            var RE_IDCHAR = RE.CharsOf(c => lettersCategories.Contains(Char.GetUnicodeCategory(c))) | RE.Symbol('_');
 
+            //keywords
+            K_CLASS = lex.DefineToken(RE.Literal("class"));
+            K_PUBLIC = lex.DefineToken(RE.Literal("public"));
+            K_STATIC = lex.DefineToken(RE.Literal("static"));
+            K_VOID = lex.DefineToken(RE.Literal("void"));
+            K_MAIN = lex.DefineToken(RE.Literal("main"));
+            K_STRING = lex.DefineToken(RE.Literal("string"));
+            K_RETURN = lex.DefineToken(RE.Literal("return"));
+            K_INT = lex.DefineToken(RE.Literal("int"));
+            K_BOOL = lex.DefineToken(RE.Literal("bool"));
+            K_IF = lex.DefineToken(RE.Literal("if"));
+            K_ELSE = lex.DefineToken(RE.Literal("else"));
+            K_WHILE = lex.DefineToken(RE.Literal("while"));
+            K_WRITELINE = lex.DefineToken(RE.Literal("System.Console.WriteLine"));
+            K_LENGTH = lex.DefineToken(RE.Literal("Length"));
+            K_TRUE = lex.DefineToken(RE.Literal("true"));
+            K_FALSE = lex.DefineToken(RE.Literal("false"));
+            K_THIS = lex.DefineToken(RE.Literal("this"));
+            K_NEW = lex.DefineToken(RE.Literal("new"));
 
-            var ID = global.DefineToken(RE_IDCHAR.Concat(
+            ID = lex.DefineToken(RE_IDCHAR.Concat(
                 (RE_IDCHAR | RE.Range('0', '9')).Many()));
-            var NUM = global.DefineToken(RE.Range('0', '9').Many1());
-            var WHITESPACE = global.DefineToken(RE.Symbol(' ').Many());
+            INTEGER_LITERAL = lex.DefineToken(RE.Range('0', '9').Many1());
+            WHITESPACE = lex.DefineToken(RE.Symbol(' ').Many());
         }
 
         protected override Compilers.Parsers.Combinators.Parser<Ast.Program> OnDefineParser()
