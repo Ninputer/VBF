b343a1a0145bf7e81c797a31c53ad490aff3f451
Fan Shi <ninputer@hotmail.com>
2012-06-03 00:15:50 +0800

    Add missing file

---
+++ src/Compilers/Compilers.Parsers.Combinators/ConvertHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace VBF.Compilers.Parsers.Combinators
+{
+    class ConvertHelper<TFrom, TTo>
+    {
+        private static Func<TFrom, TTo> s_CastFunc;
+
+        static ConvertHelper()
+        {
+            var source = Expression.Parameter(typeof(TFrom), "source");
+            s_CastFunc = Expression.Lambda<Func<TFrom, TTo>>(Expression.Convert(source, typeof(TTo)), source).Compile();
+        }
+
+        public static TTo Convert(TFrom source)
+        {
+            return s_CastFunc(source);
+        }
+    }
+}
