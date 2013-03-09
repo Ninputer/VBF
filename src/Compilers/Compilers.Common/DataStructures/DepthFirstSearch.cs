85a11cdae6e12ae9752d04fba92bc7c6462debf2
Ninputer <ninputer@hotmail.com>
2013-03-10 01:14:42 +0800

    Implements depth first search of graphs

--- src/Compilers/Compilers.Common/DataStructures/DepthFirstSearch.cs
+++ src/Compilers/Compilers.Common/DataStructures/DepthFirstSearch.cs
@@ -6,7 +6,131 @@ using System.Threading.Tasks;
 
 namespace VBF.Compilers.DataStructures
 {
-    public class DepthFirstSearch
+    public delegate IEnumerable<T> ChildrenGetter<T>(T node);
+
+    public class DepthFirstSearch<T>
     {
+        protected struct DFSInfo
+        {
+            public int PreIndex;
+            public int PostIndex;
+            public int PrevNodeIndex;
+            public int ComponentNumber;
+        }
+
+        public DepthFirstSearch(IEnumerable<T> nodes, ChildrenGetter<T> childrenGetter)
+        {
+            CodeContract.RequiresArgumentNotNull(nodes, "nodes");
+            CodeContract.RequiresArgumentNotNull(childrenGetter, "childrenGetter");
+
+            m_nodes = nodes.ToArray();
+            m_childrenGetter = childrenGetter;
+
+            m_visited = new bool[m_nodes.Length];
+            m_nodesInfo = new DFSInfo[m_nodes.Length];
+            m_nodeIndexLookup = new Dictionary<T, int>();
+
+            for (int i = 0; i < m_nodes.Length; i++)
+            {
+                m_nodeIndexLookup[m_nodes[i]] = i;
+            }
+        }
+
+        private ChildrenGetter<T> m_childrenGetter;
+        private DFSInfo[] m_nodesInfo;
+        private T[] m_nodes;
+        protected bool[] m_visited;
+        private Dictionary<T, int> m_nodeIndexLookup;
+
+        protected T[] Nodes
+        {
+            get
+            {
+                return m_nodes;
+            }
+        }
+
+        protected DFSInfo[] Nodes
+        {
+            get
+            {
+                return m_nodesInfo;
+            }
+        }
+
+        private int m_preClock;
+        private int m_postClock;
+
+        protected virtual void PreVisit(int nodeIndex)
+        {
+            m_nodesInfo[nodeIndex].ComponentNumber = m_componentNumber;
+            m_nodesInfo[nodeIndex].PreIndex = m_preClock;
+            ++m_preClock;
+        }
+
+        protected virtual void PostVisit(int nodeIndex)
+        {
+            m_nodesInfo[nodeIndex].PostIndex = m_postClock;
+            ++m_postClock;
+        }
+
+        private void Explore(int nodeIndex)
+        {
+            m_visited[nodeIndex] = true;
+            PreVisit(nodeIndex);
+
+            var children = m_childrenGetter(m_nodes[nodeIndex]);
+            foreach (var child in children)
+            {
+                var childIndex = m_nodeIndexLookup[child];
+                if (!m_visited[childIndex])
+                {
+                    m_nodesInfo[childIndex].PrevNodeIndex = nodeIndex;
+                    Explore(childIndex);
+                }
+            }
+
+            PostVisit(nodeIndex);
+        }
+
+        private int m_componentNumber;
+
+        public void Start()
+        {
+            m_visited.Initialize();
+            m_preClock = 0;
+            m_postClock = 0;
+
+            for (int i = 0; i < m_nodes.Length; i++)
+            {
+                if (!m_visited[i])
+                {
+                    ++m_componentNumber;
+                    Explore(i);
+                }
+            }
+        }
+
+        public int GetPreIndex(T node)
+        {
+            return m_nodesInfo[GetIndex(node)].PreIndex;
+        }
+
+        public int GetPostIndex(T node)
+        {
+            return m_nodesInfo[GetIndex(node)].PostIndex;
+        }
+
+        public T GetParentInDFSTree(T node)
+        {
+            return m_nodes[m_nodesInfo[GetIndex(node)].PrevNodeIndex];
+        }
+
+        private int GetIndex(T node)
+        {
+            return m_nodeIndexLookup[node];
+        }
+
+       
     }
 }
