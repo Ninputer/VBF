66b5d4e79703be426c1588608fe03f63000244a1
Ninputer <ninputer@hotmail.com>
2013-06-06 23:44:14 +0800

    Add exceptions for parsing failures

---
+++ src/Compilers/Compilers.Parsers/ParsingFailureException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VBF.Compilers.Parsers
+{
+    public class ParsingFailureException : Exception
+    {
+        public ParsingFailureException(string message) :base(message)
+        {
+
+        }
+    }
+}
