ffe6f547734ff252e64c7b0469c759b756f0d0ed
Fan Shi <ninputer@hotmail.com>
2015-10-11 00:46:12 +0800

    Add missing file

---
+++ src/Compilers/Compilers.Parsers/DefaultValueContainer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VBF.Compilers.Scanners;
+
+namespace VBF.Compilers.Parsers
+{
+    class DefaultValueContainer<T>
+    {
+        static DefaultValueContainer()
+        {
+            if (typeof(T) == typeof(Lexeme))
+            {
+                DefaultValue = (T)(object)Lexeme.CreateEmptyLexeme();
+            }
+            else
+            {
+                DefaultValue = default(T);
+            }
+        }
+
+        public static readonly T DefaultValue;
+    }
+}
